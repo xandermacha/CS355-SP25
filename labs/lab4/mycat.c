73dfd4c46e0007f69eb640c9c67b5a96a8eb4895
xandermacha <michaelbalachandar@gmail.com>
2025-03-12 19:15:25 -0400

    fix: it works now

--- labs/lab4/mycat.c
+++ labs/lab4/mycat.c
@@ -1,86 +1,102 @@
-#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-#define BUFFERSIZE 1024
-int iFlagE=0, iFlagN=0, iFlagS=0;
+#define BUFFERSIZE 2048
 
-int open_file(char *);
-void flagE_action();
-void flagN_action(int *);
+void print_file(const char *file, int flag_e, int flag_n, int flag_s, int *iteration);
 
 int main(int ac, char *av[]) {
-	if(ac<3) {
-		printf("%s [-ens] file1 [file2 ...]\n", av[0]);
-		return 1;
-	}
-	char cOpt;
-	char sBuff[BUFFERSIZE];
-	int fd;
-	int iNum_bytes_read;
-	int boolLine_start = 1, boolLine_end = 0;
-	int iteration = 1;
-	while((cOpt = getopt(ac, av, "ens")) != -1) {
-		switch(cOpt) {
-			case 'e':
-				iFlagE = 1;
-				break;
-			case 'n':
-				iFlagN = 1;
-				break;
-			case 's':
-				iFlagS = 1;
-				break;
-		}
-	}
-	if((fd = open_file(av[2])) == -1) {
-		return fd;
-	}
-	int boolLine_empty = 1, boolLine_num_printed = 0;
-	while((iNum_bytes_read=read(fd, &sBuff, BUFFERSIZE)) > 0) {
-		for(int i = 0; i<iNum_bytes_read; i++) {
-			char cCurrent = sBuff[i];
-			if (cCurrent != '\n' && cCurrent != '\t' && cCurrent != ' ') {
-				boolLine_empty = 0;
-			}
-			
-			if(boolLine_start && (!boolLine_num_printed && iFlagN)) {
-				flagN_action(&iteration);
-				boolLine_num_printed = 1;
-				iteration++;
-			}
-			if(cCurrent == '\n' && iFlagE) {
-				boolLine_start = 1;
-				boolLine_num_printed = 0;
-				flagE_action();
-			}
-			else if(cCurrent == '\n') {
-				boolLine_start = 1;
-				boolLine_num_printed = 0;
-			}
+    int flagE = 0, flagN = 0, flagS = 0;
+    if (ac < 2) {
+        printf("%s usage: [-ens] file1 [file2 ...]\n", av[0]);
+        return 1;
+    }
+    int iteration = 1;
+    char opt;
+    while ((opt = getopt(ac, av, "ens")) != -1) {
+        switch (opt) {
+            case 'e':
+                flagE = 1;
+                break;
+            case 'n':
+                flagN = 1;
+                break;
+            case 's':
+                flagS = 1;
+                break;
+            default:
+                printf("%s usage: [-ens] file1 [file2 ...]\n", av[0]);
+                return 1;
+        }
+    }
 
-			if(boolLine_empty && iFlagS) {
-				break;
-			}
-			putchar(cCurrent);
-		}
+    if (optind >= ac) {
+        printf("%s requires at least 1 input file\n", av[0]);
+        return 1;
+    }
 
-	}
-	/*printf("%s\n", av[2]);*/
-	return 0;
+    for (int i = optind; i < ac; i++) {
+        print_file(av[i], flagE, flagN, flagS, &iteration);
+    }
+    return 0;
 }
 
-void flagE_action() {
-	putchar('$');
-}
-void flagN_action(int *iteration) {
-	printf("%5d ", *iteration);
+void print_file(const char *file, int flag_e, int flag_n, int flag_s, int *iteration) {
+    int src = open(file, O_RDONLY);
+    if (src < 0) {
+        perror(file);
+        return;
+    }
+    
+    char buffer[BUFFERSIZE];
+    int numBytes;
+    
+    int line_start = 1;
+    int empty_line = 1;
+    int printed_line_number = 0;
+
+    while ((numBytes = read(src, buffer, BUFFERSIZE)) > 0) {
+        for (int i = 0; i < numBytes; i++) {
+            char c = buffer[i];
+
+            if (c == '\r') {
+                continue;
+            }
+
+            if (line_start) {
+                empty_line = 1;
+                printed_line_number = 0;
+                line_start = 0;
+            }
+
+            if (c != '\n' && c != '\t' && c != ' ') {
+                empty_line = 0;
+            }
+
+            if (c == '\n') {
+                if (!(flag_s && empty_line)) {
+                    if (!printed_line_number && flag_n) {
+                        printf("%5d ", (*iteration)++);
+                        printed_line_number = 1;
+                    }
+                    if (flag_e) {
+                        putchar('$');
+                    }
+                    putchar('\n');
+                    line_start = 1;
+                }
+            } else {
+                if (!printed_line_number && flag_n) {
+                    printf("%5d ", (*iteration)++);
+                    printed_line_number = 1;
+                }
+                putchar(c);
+            }
+        }
+
+    }
+    printf("\n");
+    close(src);
 }
 
-int open_file(char *sFile_name) {
-	int fd;
-	if((fd = open(sFile_name, O_RDONLY)) == -1) {
-		perror(sFile_name);
-	}
-	return fd;
-}
